381eb2e82b3451720d25c4948ae702975b18305f
Bohr1005 <66120864+Bohr1005@users.noreply.github.com>
2020-06-01 09:39:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from distutils.core import setup
 
 setup(
     name="bkt",
-    version=1.0,
     author='Bohr',
     author_email="shiyu.he@jyquant.com.cn",
-    packages=['bkt'], requires=['pandas', 'numpy']
 )
